3ae647424ff64aaaf1352390113e959431f7e4f6
Monica Seaman <monica.l.seaman@gmail.com>
2018-05-21 01:15:04 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,30 @@
+class Author
+
+  attr_accessor :name
+  @@post_count = 0
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def posts
+    @posts
+  end
+
+  def add_post(post)
+    @posts << post
+    @@post_count += 1
+    post.author = self
+  end
+
+  def add_post_by_title(post)
+    post = Post.new(post)
+    self.add_post(post)
+  end
+
+  def self.post_count
+    @@post_count
+  end
+
+end
